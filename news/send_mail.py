933818b6f613e7fdd167ab75bc64db81ee33fe55
mrGerber91 <lalkakapalka3@gmail.com>
2024-02-18 00:56:34 +0300

    Update v1.5

--- news/send_mail.py
+++ news/send_mail.py
@@ -1,17 +1,18 @@
 import os
 import django
+from django.core.mail import send_mail
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewPortal_v2.settings')
 django.setup()
 
-from django.core.mail import send_mail
-from django.conf import settings
+
 
 def send_test_email():
     subject = 'Тестовое сообщение'
     message = 'Это тестовое сообщение для проверки отправки электронной почты.'
     from_email = settings.EMAIL_HOST_USER
-    to_email = ['lalkakapalka3@gmail.com']  # Замените на ваш адрес электронной почты
+    to_email = ['lalkakapalka3@gmail.com']
 
     send_mail(subject, message, from_email, to_email)
 
